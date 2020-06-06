cf6ff05aff64c8b0280adbe938b7b236ddfbabb5
nuts-rice <red44443@gmail.com>
2020-06-06 19:16:20 -0400

    added inital rust-cargo build
    let's play with rust!

---
+++ guessing_game/target/debug/deps/guessing_game.d
@@ -0,0 +1,5 @@
+C:\Users\User\Desktop\codeSamples\rustSamples\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
+
+C:\Users\User\Desktop\codeSamples\rustSamples\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
+
+src\main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\Users\User\Desktop\codeSamples\rustSamples\guessing_game\target\debug\guessing_game.exe: C:\Users\User\Desktop\codeSamples\rustSamples\guessing_game\src\main.rs
