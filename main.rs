73bc6b0068a07d0a921b3fcd6f634438ce29e8cd
nuts-rice <red44443@gmail.com>
2020-06-06 21:21:07 -0400

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,45 @@
+use std :: io;
+//Like Result, Ordering is another enum, but the 
+//variants for Ordering are Less, Greater, and Equal.
+use std :: cmp :: Ordering;
+use rand::Rng;
+
+
+
+fn main() 
+{
+    println!("Guess the number!");
+    //let random gen number be rng number inbetween range of 1-100
+    let secret_number = rand::thread_rng().gen_range(1,101);
+    println!("The secret number is: {}", secret_number);
+    println!("Please input a number");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    //let guess be unsigned 32 bit number type 
+    //convert guess to u32 type 
+    //The parse method on strings parses a string into some kind of number.
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number");
+    println!("You guessed: {}", guess);
+
+    //cmp method compares 2 values
+    //returns varient of Ordering enum
+    match guess.cmp(&secret_number)
+    //use match expression to decide what to do next based on
+    //which varient of ordering is returned
+    {
+
+       /*  A match expression is made up of arms. An arm consists of a pattern and the 
+        code that should be run if the value given 
+        to the beginning of the match expression fits that arm’s pattern.  */
+
+        Ordering :: Less => println!("too small");
+        Ordering :: Greater => println!("too big");
+        Ordering :: Equal => println!("Guess is right");
+    }
+}
+
